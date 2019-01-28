3ed0358f504752ab7726b8b90fd4b867392319d4
Nyagajrr <muchokijnrbenson@gmail.com>
2019-01-28 09:14:49 +0300

    generating credentials' class

---
+++ credentials.py
@@ -0,0 +1,14 @@
+#CLASS CREDENTIALS
+class Credentials:
+    """
+    generates new user details
+    """
+
+     user_credentials = []
+
+    def __init__(self,first_name,last_name,user_name,phone_number,email,password):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_name = user_name
+        self.phone_number = number
