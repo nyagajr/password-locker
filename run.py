ea98b69495ffdc0277d597cd2a91c492f0664af8
Nyagajrr <muchokijnrbenson@gmail.com>
2019-01-29 11:27:22 +0300

    testing the first line output

--- run.py
+++ run.py
@@ -62,41 +62,41 @@ def main():
                     print(f"New Contact {f_name} {l_name} created")
                     print ('\n')
 
-                    # elif short_code == 'dc':
-                    #
-                    #         if display_contacts():
-                    #                 print("Here is a list of all your credentials")
-                    #                 print('\n')
-                    #
-                    #                 for contact in display_contacts():
-                    #                         print(f"{contact.first_name} {contact.last_name} .....{contact.phone_number}")
-                    #
-                    #                 print('\n')
-                    #         else:
-                    #                 print('\n')
-                    #                 print("You dont seem to have any contacts saved yet")
-                    #                 print('\n')
-                    #
-                    # elif short_code == 'fc':
-                    #
-                    #         print("Enter the number you want to search for")
-                    #
-                    #         search_number = input()
-                    #         if check_existing_contacts(search_number):
-                    #                 search_contact = find_contact(search_number)
-                    #                 print(f"{search_contact.first_name} {search_contact.last_name}")
-                    #                 print('-' * 20)
-                    #
-                    #                 print(f"Phone number.......{search_contact.phone_number}")
-                    #                 print(f"Email address.......{search_contact.email}")
-                    #         else:
-                    #                 print("That contact does not exist")
-                    #
-                    # elif short_code == "ex":
-                    #         print("Bye .......")
-                    #         break
-                    # else:
-                    #         print("I really didn't get that. Please use the short codes")
+                    elif short_code == 'dc':
+
+                            if display_credentials():
+                                    print("Here is a list of all your credentials")
+                                    print('\n')
+
+                                    for credentials in display_credentials():
+                                            print(f"{contact.first_name} {contact.last_name} .....{contact.phone_number}")
+
+                                    print('\n')
+                            else:
+                                    print('\n')
+                                    print("You dont seem to have any contacts saved yet")
+                                    print('\n')
+
+                    elif short_code == 'fc':
+
+                            print("Enter the number you want to search for")
+
+                            search_number = input()
+                            if check_existing_contacts(search_number):
+                                    search_contact = find_contact(search_number)
+                                    print(f"{search_contact.first_name} {search_contact.last_name}")
+                                    print('-' * 20)
+
+                                    print(f"Phone number.......{search_contact.phone_number}")
+                                    print(f"Email address.......{search_contact.email}")
+                            else:
+                                    print("That contact does not exist")
+
+                    elif short_code == "ex":
+                            print("Bye .......")
+                            break
+                    else:
+                            print("I really didn't get that. Please use the short codes")
 
 if __name__ == '__main__':
     main()
