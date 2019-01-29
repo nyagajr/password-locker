833f0c3be9362146c5ea88ecde035a943cce3c89
Nyagajrr <muchokijnrbenson@gmail.com>
2019-01-29 11:21:14 +0300

    creating functions for user details

--- user_details.py
+++ user_details.py
@@ -1,12 +1,33 @@
 #USER CLASS
 class User:
     """
-   generates new user details
+    generates new user details
     """
+    user_details = []
 
-   user_details = []
+    def save_user(self):
+
+        '''
+        save_user method saves user objects into user_details
+        '''
+
+        User.user_details.append(self)
+
+    # def delete_user(self):
+    #
+    #     '''
+    #     delete_contact method deletes a saved contact from the contact_list
+    #     '''
+    #
+    #     User.user_details.remove(self)
+
+    @classmethod
+    def display_user(cls):
+        '''
+        class method that returns the user details
+        '''
+        return cls.user_details
 
     def __init__(self,user_name,password):
-
         self.user_name = user_name
         self.password = password
