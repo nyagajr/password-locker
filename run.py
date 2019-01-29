1ed1dfcf35feb73f9e4543170e6ab8c604ed4c14
Nyagajrr <muchokijnrbenson@gmail.com>
2019-01-29 12:56:41 +0300

    correction

--- run.py
+++ run.py
@@ -3,19 +3,17 @@
 from credentials import Credentials
 from user_details import User
 
-def create_credentials(fname,lname,phone,email):
 def create_credentials(fname,lname,uname,phone,email,password):
     '''
-    Function to create a new contact
+    Function to create a new credentials
     '''
-    new_credentials = Credentials(fname,lname,phone,email)
+    new_credentials = Credentials(fname,lname,uname,phone,email,password)
     return new_credentials
 
 def save_credential(credentials):
     '''
-    Function to save contact
+    Function to save credentials
     '''
-    contact.save_credential()
     credentials.save_credential()
 
 def delete_credential(credentials):
@@ -54,6 +52,7 @@ def main():
 
                     print("user name ...")
                     u_name = input()
+
                     print("Phone number ...")
                     p_number = input()
 
@@ -63,51 +62,35 @@ def main():
                     print("password ...")
                     password = input()
 
-                    save_contacts(create_contact(f_name,l_name,p_number,e_address)) # create and save new contact.
-                    save_contacts(create_contact(f_name,l_name,u_name,p_number,e_address,password)) # create and save new contact.
+
                     save_credential(create_credentials(f_name,l_name,u_name,p_number,e_address,password))
                     print ('\n')
-                    print(f"New Contact {f_name} {l_name} created")
                     print(f"New Credential {f_name} {l_name} created")
                     print ('\n')
 
-                    elif short_code == 'dc':
+            elif short_code == 'dc':
 
-                            if display_credentials():
-                                    print("Here is a list of all your credentials")
-                                    print('\n')
+                    if display_credentials():
+                            print("Here is a list of all your credentials")
+                            print('\n')
 
-                                    for credentials in display_credentials():
-                                            print(f"{contact.first_name} {contact.last_name} .....{contact.phone_number}")
-
-                                    print('\n')
-                            else:
-                                    print('\n')
-                                    print("You dont seem to have any contacts saved yet")
-                                    print('\n')
-
-                    # elif short_code == 'fc':
-                    #
-                    #         print("Enter the number you want to search for")
-                    #
-                    #         search_number = input()
-                    #         if check_existing_contacts(search_number):
-                    #                 search_contact = find_contact(search_number)
-                    #                 print(f"{search_contact.first_name} {search_contact.last_name}")
-                    #                 print('-' * 20)
-                    #
-                    #                 print(f"Phone number.......{search_contact.phone_number}")
-                    #                 print(f"Email address.......{search_contact.email}")
-                    #         else:
-                    #                 print("That contact does not exist")
-                    #
-                    # elif short_code == "ex":
-                    #         print("Bye .......")
-                    #         break
-                    # else:
-                    #         print("I really didn't get that. Please use the short codes")
+                            for credentials in display_credentials():
                                  print(f"first name: {credentials.first_name}\n\nlast name: {credentials.last_name}\n\nuser name: {credentials.user_name}\n\nphone number: {credentials.phone_number}\n\nemail: {credentials.email}\n\nyour password: {credentials.password}")
 
+                            print('\n')
+                    else:
+                            print('\n')
+                            print("You dont seem to have any credentials saved yet")
+                            print('\n')
+
+
+
+            elif short_code == "ex":
+                            print("Thank you for using password locker")
+                            break
+
+            else:
                             print("INVALID!! Please use the short codes provided")
+
 if __name__ == '__main__':
     main()
